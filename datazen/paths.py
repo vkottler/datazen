d116aa06443fc2ba7f9686dcd593abea05419052
Vaughn Kottler <vaughnkottler@gmail.com>
2020-11-01 22:43:25 -0800

    100% coverage, minus render

--- datazen/paths.py
+++ datazen/paths.py
@@ -27,15 +27,8 @@ def get_path_list(root_abs_path: str, current_abs_path: str) -> List[str]:
     """
 
     assert len(current_abs_path) >= len(root_abs_path)
-
-    index = 0
-    for char in enumerate(root_abs_path):
-        if char[1] != current_abs_path[char[0]]:
-            break
-        index = index + 1
-    assert index > 0
-
-    return current_abs_path[index:].split(os.sep)
+    assert root_abs_path in current_abs_path
+    return current_abs_path[len(root_abs_path):].split(os.sep)
 
 
 def advance_dict_by_path(path_list: List[str], data: dict) -> dict:
