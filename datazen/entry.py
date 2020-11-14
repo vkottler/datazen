1c8e41ceee9df38773ce8d46513bb5a6ea1789e0
Vaughn Kottler <vaughnkottler@gmail.com>
2020-11-14 14:49:22 -0800

    add an option to execute from another dir

--- datazen/entry.py
+++ datazen/entry.py
@@ -6,6 +6,7 @@ datazen - This package's command-line entry-point.
 # built-in
 import argparse
 import logging
+import os
 import sys
 from typing import List
 
@@ -37,7 +38,12 @@ def main(argv: List[str] = None) -> int:
                               "'%(default)s')"))
     parser.add_argument("-c", "--clean", action="store_true",
                         help="clean the manifest's cache and exit")
+    parser.add_argument("-C", "--dir", default=os.getcwd(), dest="dir",
+                        help=("execute from a specific directory (default: " +
+                              "'%(default)s')"))
     parser.add_argument("targets", nargs="*", help="target(s) to execute")
+
+    starting_dir = os.getcwd()
 
     # parse arguments and execute the requested command
     try:
@@ -50,23 +56,28 @@ def main(argv: List[str] = None) -> int:
                             format=("%(name)-30s - %(levelname)-8s - "
                                     "%(message)s"))
 
+        # change to the specified directory
+        os.chdir(args.dir)
+
         env = from_manifest(args.manifest)
 
         # clean, if requested
         if args.clean:
             env.clean_cache()
-            return result
-
-        # execute targets
-        for target in args.targets:
-            if not env.execute(target)[0]:
-                LOG.error("target '%s' failed", target)
-                result = 1
-                break
+        else:
+            # execute targets
+            for target in args.targets:
+                if not env.execute(target)[0]:
+                    LOG.error("target '%s' failed", target)
+                    result = 1
+                    break
 
     except SystemExit as exc:
         result = 1
         if exc.code is not None:
             result = exc.code
+
+    # return to starting dir
+    os.chdir(starting_dir)
 
     return result
--- tests/test_entry.py
+++ tests/test_entry.py
@@ -3,6 +3,9 @@
 datazen - Test the program's entry-point.
 """
 
+# built-in
+import os
+
 # module under test
 from datazen import PKG_NAME
 from datazen.entry import main as datazen_main
@@ -14,11 +17,14 @@ from .resources import get_resource
 def test_entry():
     """ Test some basic command-line argument scenarios. """
 
-    args = [PKG_NAME, "-m", get_resource("manifest.yaml", True)]
+    manifest = get_resource("manifest.yaml", True)
+    manifest_dir = os.path.dirname(manifest)
+    args = [PKG_NAME, "-m", manifest]
 
     assert datazen_main(args + ["--not-an-option", "asdf"]) != 0
 
     assert datazen_main(args) == 0
     assert datazen_main(args + ["a", "b", "c"]) == 0
     assert datazen_main(args + ["not_a_target"]) != 0
+    assert datazen_main([PKG_NAME, "-C", manifest_dir, "a", "b", "c"]) == 0
     assert datazen_main(args + ["-c"]) == 0
