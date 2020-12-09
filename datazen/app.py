00e5e8f2a7f5c13e6fe7741dca15e781f791b323
Vaughn Kottler <vaughnkottler@gmail.com>
2020-12-08 16:19:30 -0800

    Use new, templated entry.py

---
+++ datazen/app.py
@@ -0,0 +1,49 @@
+
+"""
+datazen - This package's command-line entry-point application.
+"""
+
+# built-in
+import argparse
+
+# internal
+from datazen.classes.environment import from_manifest
+from datazen import DEFAULT_MANIFEST
+
+
+def entry(args: argparse.Namespace) -> int:
+    """ Execute the requested task. """
+
+    result = 0
+    env = from_manifest(args.manifest)
+    if env.get_valid():
+        # clean, if requested
+        if args.sync:
+            env.write_cache()
+        if args.clean:
+            env.clean_cache()
+        elif args.describe:
+            env.describe_cache()
+        else:
+            # execute targets
+            result = int(not env.execute_targets(args.targets))
+    else:
+        result = 1
+    return result
+
+
+def add_app_args(parser: argparse.ArgumentParser) -> None:
+    """ Add application-specific arguments to the command-line parser. """
+
+    parser.add_argument("-m", "--manifest", default=DEFAULT_MANIFEST,
+                        help=("manifest to execute tasks from (default: " +
+                              "'%(default)s')"))
+    parser.add_argument("-c", "--clean", action="store_true",
+                        help="clean the manifest's cache and exit")
+    parser.add_argument("--sync", action="store_true",
+                        help=("sync the manifest's cache (write-through) " +
+                              "with the state of the file system before " +
+                              "execution"))
+    parser.add_argument("-d", "--describe", action="store_true",
+                        help="describe the manifest's cache and exit")
+    parser.add_argument("targets", nargs="*", help="target(s) to execute")
--- datazen/entry.py
+++ datazen/entry.py
@@ -1,6 +1,11 @@
+# =====================================
+# generator=datazen
+# version=1.0.11
+# hash=4f3a065715a84d8205d216935ad1e939
+# =====================================
 
 """
-datazen - This package's command-line entry-point.
+datazen - This package's command-line entry-point (boilerplate).
 """
 
 # built-in
@@ -11,10 +16,8 @@ import sys
 from typing import List
 
 # internal
-from datazen.classes.environment import from_manifest
-from datazen import VERSION, DESCRIPTION, DEFAULT_MANIFEST
-
-LOG = logging.getLogger(__name__)
+from datazen import VERSION, DESCRIPTION
+from datazen.app import entry, add_app_args
 
 
 def main(argv: List[str] = None) -> int:
@@ -33,23 +36,12 @@ def main(argv: List[str] = None) -> int:
                         version="%(prog)s {0}".format(VERSION))
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="set to increase logging verbosity")
-    parser.add_argument("-m", "--manifest", default=DEFAULT_MANIFEST,
-                        help=("manifest to execute tasks from (default: " +
-                              "'%(default)s')"))
-    parser.add_argument("-c", "--clean", action="store_true",
-                        help="clean the manifest's cache and exit")
-    parser.add_argument("--sync", action="store_true",
-                        help=("sync the manifest's cache (write-through) " +
-                              "with the state of the file system before " +
-                              "execution"))
-    parser.add_argument("-d", "--describe", action="store_true",
-                        help="describe the manifest's cache and exit")
     parser.add_argument("-C", "--dir", default=os.getcwd(), dest="dir",
                         help=("execute from a specific directory (default: " +
                               "'%(default)s')"))
-    parser.add_argument("targets", nargs="*", help="target(s) to execute")
-
     starting_dir = os.getcwd()
+
+    add_app_args(parser)
 
     # parse arguments and execute the requested command
     try:
@@ -65,22 +57,8 @@ def main(argv: List[str] = None) -> int:
         # change to the specified directory
         os.chdir(args.dir)
 
-        env = from_manifest(args.manifest)
-
-        if env.get_valid():
-            # clean, if requested
-            if args.sync:
-                env.write_cache()
-            if args.clean:
-                env.clean_cache()
-            elif args.describe:
-                env.describe_cache()
-            else:
-                # execute targets
-                result = int(not env.execute_targets(args.targets))
-        else:
-            result = 1
-
+        # run the application
+        result = entry(args)
     except SystemExit as exc:
         result = 1
         if exc.code is not None:
