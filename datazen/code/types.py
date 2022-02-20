11f25d33dd7ef2c8df9a4ea694c1e2ae5a92b4c2
Vaughn Kottler <vaughnkottler@gmail.com>
2022-02-20 01:22:42 -0800

    1.13.0 - Add archiving to directory cache

--- datazen/code/types.py
+++ datazen/code/types.py
@@ -3,12 +3,80 @@ datazen - Common type definitions for encoding and decoding interfaces.
 """
 
 # built-in
+from enum import Enum
 from io import StringIO
 from logging import Logger
-from typing import Callable, NamedTuple, TextIO, Tuple, Union
+from pathlib import Path
+from typing import (
+    Callable,
+    FrozenSet,
+    NamedTuple,
+    Optional,
+    TextIO,
+    Tuple,
+    Union,
+)
 
 # third-party
 from ruamel.yaml import YAML
+
+# internal
+from datazen.paths import get_file_ext
+
+
+class FileExtension(Enum):
+    """A mapping of expected encoding type to file extensions."""
+
+    UNKNOWN: FrozenSet[str] = frozenset()
+    JSON: FrozenSet[str] = frozenset(["json"])
+    YAML: FrozenSet[str] = frozenset(["yaml", "yml", "eyaml"])
+    INI: FrozenSet[str] = frozenset(["ini", "cfg"])
+    ZIP: FrozenSet[str] = frozenset(["zip"])
+    TAR: FrozenSet[str] = frozenset(
+        ["tar", "tar.gz", "tar.bz2", "tar.lzma", "tar.xz"]
+    )
+
+    def is_archive(self) -> bool:
+        """Determine if this extension is a kind of archive file."""
+        return self in {FileExtension.ZIP, FileExtension.TAR}
+
+    @staticmethod
+    def has_archive(path: Path) -> Optional[Path]:
+        """Determine if a path has an associated archive file."""
+
+        for ext in [FileExtension.ZIP, FileExtension.TAR]:
+            for ext_str in list(ext.value):
+                check_path = Path(f"{path}.{ext_str}")
+                if check_path.is_file():
+                    return check_path
+
+        return None
+
+    def is_data(self) -> bool:
+        """Determine if this etension is a kind of data file."""
+        return self in {
+            FileExtension.JSON,
+            FileExtension.YAML,
+            FileExtension.INI,
+        }
+
+    @staticmethod
+    def from_ext(ext_str: str) -> Optional["FileExtension"]:
+        """Given a file extension, determine what kind of file it is."""
+
+        result = FileExtension.UNKNOWN
+        for ext in FileExtension:
+            if ext_str in ext.value:
+                result = ext
+                break
+        return None if result is FileExtension.UNKNOWN else result
+
+    @staticmethod
+    def from_path(
+        path: Union[Path, str], maxsplit: int = 1
+    ) -> Optional["FileExtension"]:
+        """Get a known file extension for a path, if it exists."""
+        return FileExtension.from_ext(get_file_ext(path, maxsplit=maxsplit))
 
 
 class LoadResult(NamedTuple):
