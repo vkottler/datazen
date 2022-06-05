833e4846f563f28b27eb238205af822010a5310a
Vaughn Kottler <vaughnkottler@gmail.com>
2022-06-04 23:07:59 -0700

    Update Github CI for Windows

--- tests/environment/test_command.py
+++ tests/environment/test_command.py
@@ -2,6 +2,9 @@
 datazen - Tests for the 'CommandEnvironment' class mixin.
 """
 
+# third-party
+from vcorelib.task.subprocess.run import is_windows
+
 # module under test
 from datazen.environment.integrated import from_manifest
 
@@ -19,20 +22,21 @@ def test_command_duplicate_matches():
 def test_command_basic():
     """Test basic commanding functionality."""
 
-    with scoped_environment() as env:
-        # run a command
-        assert env.command("a") == (True, True)
-        assert env.command("b") == (True, True)
-        assert env.command("a") == (True, False)
-        assert env.command("false") == (False, True)
+    if not is_windows():
+        with scoped_environment() as env:
+            # run a command
+            assert env.command("a") == (True, True)
+            assert env.command("b") == (True, True)
+            assert env.command("a") == (True, False)
+            assert env.command("false") == (False, True)
 
-        # test templated commands
-        assert env.command("echo-asdf") == (True, True)
-        assert env.command("echo-asdf") == (True, False)
-        assert env.command("echo-asdfasdf") == (True, True)
-        assert env.command("echo-asdfasdf") == (True, False)
-        assert env.command("ECHO-asdf") == (False, False)
+            # test templated commands
+            assert env.command("echo-asdf") == (True, True)
+            assert env.command("echo-asdf") == (True, False)
+            assert env.command("echo-asdfasdf") == (True, True)
+            assert env.command("echo-asdfasdf") == (True, False)
+            assert env.command("ECHO-asdf") == (False, False)
 
-        env.write_cache()
-        new_env = from_manifest(get_resource("manifest.yaml", True))
-        assert new_env.command("a") == (True, False)
+            env.write_cache()
+            new_env = from_manifest(get_resource("manifest.yaml", True))
+            assert new_env.command("a") == (True, False)
