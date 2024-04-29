ae9c8b70856137e636de60dedb789d55544296e0
Vaughn Kottler <vaughnkottler@gmail.com>
2024-04-28 22:54:31 -0500

    Update boilerplate

--- tests/resources.py
+++ tests/resources.py
@@ -10,36 +10,37 @@ from tempfile import NamedTemporaryFile, TemporaryDirectory
 from typing import Iterator, List, TextIO
 
 # third-party
-from git import Repo  # type: ignore
-import pkg_resources
+from git import Repo
 
 # module under test
 from datazen.environment.integrated import Environment, from_manifest
 
+TESTS = Path(__file__).parent
+
 
 def get_resource(
-    resource_name: str, valid: bool = True, pkg: str = __name__
+    resource_name: str | Path, valid: bool = True, pkg: str = __name__
 ) -> str:
     """Locate the path to a test resource."""
 
-    valid_str = "valid" if valid else "invalid"
-    resource_path = os.path.join("data", valid_str, resource_name)
-    return pkg_resources.resource_filename(pkg, resource_path)
+    # Can't search in other packages.
+    assert pkg == __name__
+
+    return str(
+        TESTS.joinpath("data", "valid" if valid else "invalid", resource_name)
+    )
 
 
-def get_archives_root(pkg: str = __name__) -> Path:
+def get_archives_root() -> Path:
     """Get the data directory for test archives."""
-    resource = os.path.join("data", "archives")
-    return Path(pkg_resources.resource_filename(pkg, resource))
+    return TESTS.joinpath("data", "archives")
 
 
 def get_scenario_manifest(
-    scenario: str, manifest: str = "manifest.yaml", pkg: str = __name__
+    scenario: str, manifest: str = "manifest.yaml"
 ) -> Path:
     """Get the path to a manifest in a given test scenario."""
-
-    resource = os.path.join("data", "scenarios", scenario, manifest)
-    return Path(pkg_resources.resource_filename(pkg, resource))
+    return TESTS.joinpath("data", "scenarios", scenario, manifest)
 
 
 @contextmanager
